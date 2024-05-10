7de5acf53a8e0b106bf4acde0d0e01846f93c91a
dr-monocle <francisco@franciscoliveira.com>
2024-05-10 18:28:24 +0100

    feat: Improve error message and retry logic in main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from time import sleep
 from src.methods import initialize, openAd
 from src.colors import bcolors
+from datetime import datetime
 
 def main(username, password, headless=False):
   while True:
@@ -8,7 +9,10 @@ def main(username, password, headless=False):
     tryAgainIn = openAd(driver)
     
     if (tryAgainIn != -1):
-      print(bcolors.FAIL + "Ad not available. Trying again in " + str(tryAgainIn) + " minute(s).\n(DON'T CLOSE THIS WINDOW)" + bcolors.ENDC)
+      now = datetime.now()
+      nextTry = now.replace(minute=now.minute + tryAgainIn)
+      
+      print(bcolors.FAIL + "Ad not available. Trying again at " + nextTry.strftime("%H:%M") + " (in " + str(tryAgainIn) + " minute(s)).\n(DON'T CLOSE THIS WINDOW)" + bcolors.ENDC)
       driver.quit()
       sleep(tryAgainIn * 60)
       continue
