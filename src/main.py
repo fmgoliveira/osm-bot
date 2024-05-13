c6095792d0dc48381753edec1cf0484f88a70ba1
dr-monocle <francisco@franciscoliveira.com>
2024-05-13 20:27:21 +0100

    Refactor tryAgainIn calculation in main.py

--- src/main.py
+++ src/main.py
@@ -10,7 +10,10 @@ def main(username, password, headless=False):
     
     if (tryAgainIn != -1):
       now = datetime.now()
-      nextTry = now.replace(minute=now.minute + tryAgainIn)
+      if ((now.minute + tryAgainIn) > 59):
+        nextTry = now.replace(hour = now.hour + 1, minute = (now.minute + tryAgainIn) - 60)
+      else:
+        nextTry = now.replace(minute = now.minute + tryAgainIn)
       
       print(bcolors.FAIL + "Ad not available. Trying again at " + nextTry.strftime("%H:%M") + " (in " + str(tryAgainIn) + " minute(s)).\n(DON'T CLOSE THIS WINDOW)" + bcolors.ENDC)
       driver.quit()
